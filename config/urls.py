0306b00b92a553c345cd1ec05fcb5f756a69ed61
artemlira <artemlira84@gmail.com>
2020-07-03 21:24:06 +0300

    add author

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bookstore/', include('bookstore.urls'))
+    path('api/', include('bookstore.urls'))
 ]
